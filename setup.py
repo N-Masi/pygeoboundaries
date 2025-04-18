74b1be39b9a1ae2bd7877293f3e828cced3f4be2
N-Masi <nickmasi17@gmail.com>
2025-04-18 14:01:48 -0400

    refactor: package organization

--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from distutils.core import setup
 setup(name='pygeoboundaries_geolab',
       version='0.0.2',
       py_modules=['pygeoboundaries'],
+      install_requires=['geojson', 'requests_cache']
       )
